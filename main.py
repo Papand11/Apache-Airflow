7ae34240d813ceade8461bde21c3fe2300a821b7
Danil <dan.sartak@gmail.com>
2025-03-12 11:53:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import liquibase.Liquibase;
 import liquibase.database.Database;
 import liquibase.database.jvm.HibernateDatabase;
 import liquibase.resource.ClassLoaderResourceAccessor;
+import java.sql.Connection;
 import java.util.logging.Logger;
 
 public class MigrationManager {
@@ -12,26 +14,4 @@ public class MigrationManager {
 
     public static void main(String[] args) {
         try {
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-            runMigrations();
-        } catch (Exception e) {
-            logger.severe("Database migration failed: " + e.getMessage());
-            throw new RuntimeException("Migration error", e);
-        }
-    }
-
-    public static void runMigrations() {
-        try {
-            Database database = new HibernateDatabase();
-            database.setConnection(sessionFactory.getSessionFactoryOptions().getServiceRegistry()
-                    .getService(org.hibernate.engine.jdbc.connections.spi.ConnectionProvider.class)
-                    .getConnection());
-
-            Liquibase liquibase = new Liquibase("db/changelog.xml", new ClassLoaderResourceAccessor(), database);
-            liquibase.update("");
-            logger.info("Database migrations applied successfully.");
-        } catch (Exception e) {
-            logger.severe("Migration error: " + e.getMessage());
-        }
-    }
-}
+            sessionFactory = new Configuration().configure("
